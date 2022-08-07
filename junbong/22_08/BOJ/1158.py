e07810fed91af8f4e518732ebd81638b38c9267f
becoding96 <becoding96@gmail.com>
2022-08-07 17:15:54 +0900

    백준봉 220807

--- junbong/22_08/BOJ/1158.py
+++ junbong/22_08/BOJ/1158.py
@@ -2,6 +2,18 @@
 import sys
 from collections import deque
 
+'''
+1. 큐 문제인 이유
+큐: 선입선출-FIFO(First In First Out)
+원을 따라 계속해서 순회하면서 매 k번째마다 뽑아내는 방식은
+큐에서 매 k번째 순서가 아니면 뽑아내서 다시 큐에 넣고
+k번째는 뽑아서 결과에 저장하는 방식과 같다.
+따라서 큐를 사용하면 편하게 해결할 수 있다.
+
+2. 풀이
+q에 원소가 남아있는 동안 1번의 방식대로 진행
+'''
+
 input = sys.stdin.readline
 n, k = map(int, input().split())  # 입력이 한 줄에 끝나므로 rstrip() 필요 없는 듯
 q = deque(list(range(1, n + 1)))  # 1 ~ n
--- junbong/22_08/BOJ/9012.py
+++ junbong/22_08/BOJ/9012.py
@@ -2,6 +2,19 @@
 import sys
 from collections import deque
 
+'''
+1. 스택 문제인 이유
+닫는 괄호가 나오면 가장 최근의 여는 괄호가 닫힌다.
+스택의 개념인 후입선출-LIFO(Last In First Out) 방식.
+
+2. 풀이
+여는 괄호가 나오면 스택에 넣고, 닫는 괄호가 나오면 뺀다.
+여는 괄호의 개수 초과로 닫는 괄호가 나오는 '순간' 괄호 문자열이 아니게 되므로 NO 출력.
+문자열을 '다 탐색한 이후' 여는 괄호의 개수 미만으로 닫는 괄호가 나오면
+괄호 문자열이 아니므로 NO 출력.
+이외에는 (여는 괄호가 먼저 나오면서, 닫는 괄호와 개수가 같으면) YES를 출력한다.
+'''
+
 # 함수도 객체이므로 아래와 같이 저장 가능
 input = sys.stdin.readline  # 더 빠르게 입력
 
@@ -13,13 +26,14 @@ for _ in range(int(input().rstrip())):  # readline은 개행문자 '\n'을 포�
         if p == '(':
             stack.append(p)
         else:
-            try:
+            try:  # 속도 측면에서 if보다 빠르다고 생각해서 사용
                 stack.pop()
             except:  # 예외시 실행할 부분
                 flag = True
                 break  # 더 이상 볼 필요 없으므로 break
 
-    if flag or len(stack):  # 열리지도 않은 괄호를 닫으려 했거나, 열린 괄호를 다 닫지 않았으면
+    # 열리지도 않은 괄호를 닫으려 했거나, 열린 괄호를 다 닫지 않았으면
+    if flag or len(stack):
         print("NO")
         continue
 
