e1b29a2138bd7ff074d8e1c9941b83b9ec7f1d2b
dmswls <eunjin@eujin.com>
2022-10-04 20:04:52 +0900

    양은진 221004

--- eunjin/22_10/BOJ/14889.py
+++ eunjin/22_10/BOJ/14889.py
@@ -1,32 +1,35 @@
 import sys
 input = sys.stdin.readline
 
-def f(i,k):
+def c(a, b):
     global ans
-    if k == n//2:
+    if b == 0:
         start = 0
+        # print(p)
         for i in range(n):
             for j in range(n):
-                if arr[i] == 1 and arr[j] == 1:
+                if i in p and j in p:
                     start += nums[i][j]
-                elif not arr[i] == 1 and not arr[j] == 1:
-                    start -= nums[i][j] 
+                    # print(f'+:{i}{j}{start}')
+                elif (i not in p) and (j not in p):
+                    start -= nums[i][j]
+                    # print(f'-:{i}{j}{start}')
+        # print(f'start:{start}')
         if abs(start) <= ans:
             ans = abs(start)
+    elif a < b:
         return
-    for u in range(i,n):
-        if arr[u] == 0:
-            arr[u] = 1
-            f(i+1,k+1)
-            arr[u] = 0
-        
+    else:
+        p[b-1] = arr[a-1]
+        c(a-1, b-1)
+        c(a-1, b)
 
 
 n =  int(input())
 nums = [list(map(int, input().split())) for _ in range(n)]
 
-arr = [0] * n
+p = [0] * (n//2)
 ans = float("inf")
-
-f(0,0)
+arr = [x for x in range(n)]
+c(n, n//2)
 print(ans)
