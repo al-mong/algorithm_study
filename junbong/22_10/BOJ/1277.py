0969884238fd8cd2b147c90cadb4c870cc89f445
becoding96 <becoding96@gmail.com>
2022-10-17 20:40:44 +0900

    [백준봉] 알몽 10주차 - 다익스트라

--- junbong/22_10/BOJ/1277.py
+++ junbong/22_10/BOJ/1277.py
@@ -1,4 +1,3 @@
-# 221014
 import sys; input = sys.stdin.readline
 
 
@@ -12,7 +11,7 @@ def dijkstra(s):
         minV = 300000
         w = 0
         for i in range(1, N + 1):
-            if U[i] == 0 and 0 <= D[i] < minV:
+            if U[i] == 0 and 0 <= D[i] < minV:  # 0도 포함
                 minV = D[i]
                 w = i
         U[w] = 1
@@ -24,13 +23,17 @@ def dijkstra(s):
 N, W = map(int, input().split())
 M = float(input().rstrip())
 points = [()] + [tuple(map(int, input().split())) for _ in range(N)]
+# 이미 이어진 발전소끼리는 거리를 0으로 표시하는데,
+# 0은 유효한 숫자이므로 -1로 초기화
 board = [[-1] * (N + 1) for _ in range(N + 1)]
 
+# 거리 계산
 for i in range(1, N + 1):
     for j in range(i + 1, N + 1):
         if i != j:
             board[i][j] = board[j][i] = ((points[i][0] - points[j][0]) ** 2 + (points[i][1] - points[j][1]) ** 2) ** (1/2)
 
+# 이미 이어진 발전소끼리의 거리를 0으로
 for _ in range(W):
     a, b = map(int, input().split())
     board[a][b] = board[b][a] = 0
