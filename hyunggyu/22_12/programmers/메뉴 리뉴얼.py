30fe93f046b063236e2d2d6b388a65983ce6dec4
goeom77 <7eom14@gmail.com>
2022-12-03 13:25:42 +0900

    [엄형규]알몽 14주차 - 프로그래머스 2021 KAKAO BLIND

---
+++ hyunggyu/22_12/programmers/메뉴 리뉴얼.py
@@ -0,0 +1,44 @@
+orders = ["XYZ", "XWY", "WXA"]
+course = [2, 3, 4]
+new_menu = [[] for _ in range(11)] # course마다 가능 메뉴 10까지만 가능
+new_menu_count = {} # munu들의 count
+def comb(order,n,m,j):
+    if n == m:
+        str_chosen = ''.join(s for s in chosen[:])
+        if str_chosen in new_menu[m]:
+            new_menu_count[str_chosen] += 1
+            return
+        else:
+            new_menu[m].append(str_chosen) # new_menu의 길이 m 중에 조합가능 메뉴
+            new_menu_count[str_chosen] = 1
+            return
+    else:
+        for i in range(j,len(order)):
+            chosen[n] = order[i]
+            comb(order,n+1,m,i+1)
+            chosen[n] = 0 # 이게 있어야지만 백트래킹을 하는 걸까?
+for count in course: # 코스의 개수 count 1~10
+    for order in orders:
+        chosen = [0]*count
+        order = list(order)
+        order.sort()
+        comb(order,0,count,0)
+result = []
+min_course_num = [0 for _ in range(11)]
+for i in course:
+    tmp = []
+    for case in new_menu[i]:
+        if new_menu_count[case] > 1:
+            tmp.append([-new_menu_count[case],case])
+            if -new_menu_count[case] < min_course_num[i]:
+                min_course_num[i] = -new_menu_count[case]
+    tmp.sort()
+    for j in tmp:
+        if j[0] == min_course_num[i]:
+            result.append(j[1])
+result.sort()
+print(result)
+
+            
+        
+    
---
+++ hyunggyu/22_12/programmers/신규아이디추천.py
@@ -0,0 +1,48 @@
+def solution(new_id):
+    # 1. 대문자 -> 소문자
+    new_id = new_id.lower()
+    # 2. 소문자, 숫자, 빼기, 밑줄, 마침표 제외 제거
+    renew_id = ''
+    zero = 48
+    nine = 57
+    alpa_a = 97
+    alpa_z = 122
+    doubledot = True # 제일 처음 dot 제거
+    for i in new_id:
+        # 0~9 : 48~57
+        # a~z : 97~122
+        if doubledot == False and i == '.':
+            renew_id = renew_id + i
+            doubledot = True
+        elif i == '_':
+            renew_id = renew_id + i
+            doubledot = False
+        elif i == '-':
+            renew_id = renew_id + i
+            doubledot = False
+        elif zero <= ord(i) <= nine:
+            renew_id = renew_id + i
+            doubledot = False
+        elif alpa_a <= ord(i) <= alpa_z:
+            renew_id = renew_id + i
+            doubledot = False
+    # 3. .이 2개 이상 -> . 1개
+    # 4. .이 시작 또는 끝일 경우 제거
+    if renew_id != '' and renew_id[-1] == '.':
+        renew_id = renew_id[:-1]
+    # 5. ''빈문자열 이면 "a" 대입
+    word_length = len(renew_id)
+    if word_length == 0:
+        renew_id = 'aaa'
+    # 6. 16자 이상이면 앞 15개만 놔두기 + 마지막 . 이면 제거
+    elif word_length > 15:
+        renew_id = renew_id[:15]
+        if renew_id[-1] == '.':
+            renew_id = renew_id[:-1]
+    # 7. 2자 이하이면 마지막 문자 반복 -> 3글자가 될때까지 -> '.' 이면?
+    elif word_length == 1:
+        renew_id = renew_id*3
+    elif word_length == 2:
+        renew_id = renew_id + renew_id[1]
+    answer = renew_id
+    return answer
