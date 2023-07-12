6dc62ae78bf90206b72b641f3a0ae887d728d575
Hello1Robot <hello11robot@gmail.com>
2023-07-12 11:16:51 +0900

    [최권민] 알몽 36주차 - 프로그래머스 카펫, 예상 대진표

--- kwonmin/23_07/programmers/12985.java
+++ kwonmin/23_07/programmers/12985.java
@@ -1,19 +1,17 @@
 // A가 B보다 항상 크다는 가정이 없음 = 그냥 바꿔주는 게 좋을듯?
 
-class 예상대진표
-{
-	public int solution(int n, int a, int b)
-	{
+class Solution {
+	public int solution(int n, int a, int b) {
 		int answer = 0;
-		int new_a = Math.min(a,b);
-		int new_b = Math.max(a,b);
-		while(true){
-			answer+=1;
-			if (a%2 == 1 && b == a+1){
+		int new_a = Math.min(a, b);
+		int new_b = Math.max(a, b);
+		while (true) {
+			answer += 1;
+			if (new_a % 2 == 1 && new_b == new_a + 1) {
 				break;
 			}
-			a = (int)Math.ceil((double)a/2);
-			b = (int)Math.ceil((double)b/2);
+			new_a = (int) Math.ceil((double) new_a / 2);
+			new_b = (int) Math.ceil((double) new_b / 2);
 		}
 
 		return answer;
