2dd03251600d874d4bd3dff5fe69658b7e9066da
Seungtae Ryu <81846487+SeungtaeRyu@users.noreply.github.com>
2023-10-19 16:03:04 +0900

    [류승태] 알몽 50주차 - 2020 카카오 인턴십 경주로 건설

--- seungtae/23_10/programmers/67259.java
+++ seungtae/23_10/programmers/67259.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 class Solution {
 	private int[][][] visited;
 	public int solution(int[][] board) {
+		visited = new int[board.length][board.length][4];   			// visited 배열 선언 [x][y][상,하,좌,우]
+
 		// visited 배열 초기화
-		visited = new int[board.length][board.length][4];   // visited 배열 선언 [x][y][상,하,좌,우]
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board.length; j++) {
 				Arrays.fill(visited[i][j], Integer.MAX_VALUE);
@@ -22,11 +23,11 @@ class Solution {
 	private void dfs(int[][] board, int x, int y, int direction, int value) {
 		// direction 은 상하좌우 [상, 하, 좌, 우]
 		// [상] x 증가, direction = 0
-		if (x+1 < board.length && board[x+1][y] == 0) {                 // 인덱스 검사 && 벽이 아닌지 검사
-			int cost = direction == 0 || direction == 1 ? 100 : 600;    // 이전 direction 과 비교하여 다음에 상으로 갈때의 cost 계산
-			if (value + cost < visited[x+1][y][0]) {                    // 계산된 cost가 visited에 저장된 값보다 작으면
+		if (x+1 < board.length && board[x+1][y] == 0) {                 	// 인덱스 검사 && 벽이 아닌지 검사
+			int cost = direction == 0 || direction == 1 ? 100 : 600;    	// 이전 direction 과 비교하여 다음에 상으로 갈때의 cost 계산
+			if (value + cost < visited[x+1][y][0]) {                    	// 계산된 cost가 visited에 저장된 값보다 작으면
 				visited[x+1][y][0] = value + cost;                      // visited 갱신 및
-				dfs(board, x+1, y, 0, value + cost);    // dfs 이동
+				dfs(board, x+1, y, 0, value + cost);    		// dfs 이동
 			}
 		}
 		// [하] x 감소, direction = 1
